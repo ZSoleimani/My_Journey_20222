7eadd0ef25dcd575ad0afd53f230edb1a31b3c80
Zahra <s170422@student.dtu.dk>
2022-05-22 10:13:47 +0200

    learning points added

---
+++ Lesson_3/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// boolean: true false
+// string "..." '...'
+// integer 2 3 4
+// double 2.3
+
+
+// $a = 4;
+// $b = 10;
+
+// $c = $a * $b / 5;
+
+// echo $c;
+
+
+// and or not
+
+// $age = 40;
+// $gender = true;
+
+// if ($age < 18 && $gender == true) {
+//     echo "Under 18";
+// } else if($age < 30) {
+//     echo "Btw 18 and 30";
+// } else if ($age < 80) {
+//     echo "Btw 30 and 80";
+// } else {
+//     echo "Larger than 80";
+// }
+
+// echo "End of code";
+
+// if ($user->premium == true || $user->credit > $order->sum) {
+
+// }
